5ba8926e3c069345751676f6f75e38b66bad2118
Andrey Dorodnev <andrew992@mail.ru>
2020-06-10 11:58:52 +0400

    add routes paths

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const SIGN_UP = '/signup';
+export const SIGN_OUT = '/signout';
+export const HOME = '/home';
+export const PROFILE = '/profile';
+export const CHANGE_PASSWORD = '/pw-change';
+export const RESET_PASSWORD = '/pw-reset';
